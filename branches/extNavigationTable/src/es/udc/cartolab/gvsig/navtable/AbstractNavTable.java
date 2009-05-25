85640f017b859d3a2b64ad5e20045d74ccc8894d
Nacho Varela <nachouve@gmail.com>
2009-05-25 11:26:44 +0000

    After save() fillValues() is called, so if some values are not saved user can view it.

--- branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AbstractNavTable.java
+++ branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AbstractNavTable.java
@@ -619,6 +619,7 @@ public abstract class AbstractNavTable extends JPanel implements IWindow, Action
 		}
 		if (e.getSource() == saveB){
 			saveRegister();
+			fillValues(this.currentPosition);
 		}
 		refreshGUI();
 
