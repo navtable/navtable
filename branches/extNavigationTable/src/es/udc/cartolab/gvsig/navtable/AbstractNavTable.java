9995aa82288cce7eff5d667fcfa517eec2969399
Nacho Varela <nachouve@gmail.com>
2009-05-29 08:11:32 +0000

    Infinite bucle refreshing when select button is pressed fixed!

--- branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AbstractNavTable.java
+++ branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AbstractNavTable.java
@@ -724,16 +724,17 @@ public abstract class AbstractNavTable extends JPanel implements IWindow, Action
 
 	public void selectionChanged(SelectionEvent e) {
 		if (onlySelectedCB.isSelected()) {
-		FBitSet bitset = null;
-		int pos = Long.valueOf(currentPosition).intValue();
-		//TODO gvSIG comment: Esta comprobacion se hacia con Selectable
-		if (layer instanceof AlphanumericData) {                                     
+			FBitSet bitset = null;
+			int pos = Long.valueOf(currentPosition).intValue();
+			//TODO gvSIG comment: Esta comprobacion se hacia con Selectable
+			if (layer instanceof AlphanumericData) {                                     
 				bitset = recordset.getSelection();
 				if (!bitset.get(pos)){
 					firstSelected();
 				}
+			}
+		} else {
+			refreshGUI();
 		}
-		}
-		refreshGUI();
 	}
 }
