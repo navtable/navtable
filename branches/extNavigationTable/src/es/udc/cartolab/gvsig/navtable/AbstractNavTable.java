a6da734db2e4fa0cac5db918f700a8bb5d678cdd
Nacho Varela <nachouve@gmail.com>
2009-09-28 17:08:43 +0000

    Setting the name of the non-geographical table on the title of NavTable. Now alphanumeric tables has a '*' char on the title.

--- branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AbstractNavTable.java
+++ branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AbstractNavTable.java
@@ -134,14 +134,10 @@ public abstract class AbstractNavTable extends JPanel implements IWindow, Action
 		this.layer = null;
 		WindowInfo window = this.getWindowInfo();
 		String title = window.getTitle();
-		window.setTitle(title+": "+" NavTable de DBFs sueltos" /*layer.getName()*/);
-//		try {
-			this.recordset = recordset;
-			this.recordset.addSelectionListener(this);
-//		} catch (DriverException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		window.setTitle(title+"*: "+ recordset.getName());
+
+		this.recordset = recordset;
+		this.recordset.addSelectionListener(this);
 
 	}
 
