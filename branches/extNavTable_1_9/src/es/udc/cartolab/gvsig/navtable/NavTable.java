5a1080697a8af68db095defbd7b93e26e7a85e32
Nacho Varela <nachouve@gmail.com>
2010-03-26 19:22:43 +0000

    Change visibility of checkChangedValues() to 'public'

--- branches/extNavTable_1_9/src/es/udc/cartolab/gvsig/navtable/NavTable.java
+++ branches/extNavTable_1_9/src/es/udc/cartolab/gvsig/navtable/NavTable.java
@@ -24,16 +24,13 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
 import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
-import com.hardcode.gdbms.engine.data.driver.DriverException;
 import com.hardcode.gdbms.engine.values.NullValue;
 import com.hardcode.gdbms.engine.values.Value;
-import com.hardcode.gdbms.engine.values.ValueFactory;
 import com.hardcode.gdbms.engine.values.ValueWriter;
 import com.iver.andami.PluginServices;
 import com.iver.andami.ui.mdiManager.IWindow;
 import com.iver.andami.ui.mdiManager.WindowInfo;
 import com.iver.cit.gvsig.fmap.core.IGeometry;
-import com.iver.cit.gvsig.fmap.drivers.DriverIOException;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 import com.iver.cit.gvsig.fmap.layers.ReadableVectorial;
 import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
@@ -42,16 +39,14 @@ import com.iver.cit.gvsig.fmap.layers.VectorialFileAdapter;
 import com.iver.cit.gvsig.fmap.layers.layerOperations.AlphanumericData;
 import com.vividsolutions.jts.geom.Geometry;
 
-import es.udc.cartolab.gvsig.navtable.ToggleEditing;
-
 /**
  * <p>NavTable's main panel is a two-column table that shows
- * data row of the layer. The first column contains the 
+ * data row of the layer. The first column contains the
  * attributes names and the second one contais its values.</p>
  * 
  * <img src="images/NavTable.png" />
  *
- * <p>NOTE: the <i>data table</i> is the original data storage, not 
+ * <p>NOTE: the <i>data table</i> is the original data storage, not
  * the table to be shown in this window.</p>
  * 
  * @author Nacho Varela
@@ -63,10 +58,10 @@ public class NavTable extends AbstractNavTable {
 	private IWindow window;
 
 	protected WindowInfo viewInfo = null;
-	
+
 	protected JTable table = null;
 	private AttribTableCellRenderer cellRenderer = null;
-		
+
 	public NavTable(FLyrVect layer) {
 		super(layer);
 	}
@@ -82,13 +77,14 @@ public class NavTable extends AbstractNavTable {
 	 * 
 	 * @return the panel.
 	 */
+	@Override
 	public JPanel getCenterPanel(){
 
 		GridBagLayout glayout = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();		
+		GridBagConstraints c = new GridBagConstraints();
 
 		c.weightx = 1.0;
-		c.weighty = 1.0;				
+		c.weighty = 1.0;
 		c.fill = GridBagConstraints.BOTH;
 
 		centerPanel = new JPanel(glayout);
@@ -99,13 +95,13 @@ public class NavTable extends AbstractNavTable {
 			public void keyPressed(KeyEvent e) {
 			}
 			public void keyReleased(KeyEvent e) {
-				//TODO If control + cursor ---> Inicio / Fin				
+				//TODO If control + cursor ---> Inicio / Fin
 				if (e.getKeyCode() == KeyEvent.VK_RIGHT){
 					next();
 				}
 				if (e.getKeyCode() == KeyEvent.VK_LEFT){
 					before();
-				}				
+				}
 				if (e.getKeyCode() == KeyEvent.VK_HOME){
 					first();
 				}
@@ -115,41 +111,42 @@ public class NavTable extends AbstractNavTable {
 			}
 			public void keyTyped(KeyEvent e) {
 			}
-		});		
-		
+		});
+
 		model.addTableModelListener(new TableModelListener(){
 			//TODO
 			public void tableChanged(TableModelEvent e) {
 				//System.out.println(e.getType() );
-//				if (e.getType() == TableModelEvent.UPDATE){
-//				hasChanged = true;
-//				}
+				//				if (e.getType() == TableModelEvent.UPDATE){
+				//				hasChanged = true;
+				//				}
 			}
-		});		
-		
-		this.cellRenderer = new AttribTableCellRenderer();
-		
-		model.addColumn(PluginServices.getText(this, "headerTableAttribute"));
-		model.addColumn(PluginServices.getText(this, "headerTableValue"));		
+		});
 
-		TableColumn attribColumn = table.getColumn(PluginServices.getText(this,"headerTableAttribute"));		
+		this.cellRenderer = new AttribTableCellRenderer();
+
+		model.addColumn(PluginServices.getText(this, "headerTableAttribute"));
+		model.addColumn(PluginServices.getText(this, "headerTableValue"));
+
+		TableColumn attribColumn = table.getColumn(PluginServices.getText(this,"headerTableAttribute"));
 		attribColumn.setCellRenderer(this.cellRenderer);
 		attribColumn = table.getColumn(PluginServices.getText(this,"headerTableValue"));
 		attribColumn.setCellRenderer(this.cellRenderer);
 
-		
-		JScrollPane scrollPane = new JScrollPane(table);			
+
+		JScrollPane scrollPane = new JScrollPane(table);
 		centerPanel.add(scrollPane, c);
-		centerPanel.setMinimumSize(new Dimension(300, 400));		
+		centerPanel.setMinimumSize(new Dimension(300, 400));
 		return centerPanel;
 
 	}
 
 
+	@Override
 	public boolean init() {
-		
+
 		window = PluginServices.getMDIManager().getActiveWindow();
-				
+
 		try {
 			if (recordset.getRowCount() <= 0){
 				JOptionPane.showMessageDialog(this, PluginServices.getText(this, "emptyLayer"));
@@ -182,7 +179,7 @@ public class NavTable extends AbstractNavTable {
 		fillAttributes();
 		super.add(centerPanel, c);
 
-		c.gridy = 11;		
+		c.gridy = 11;
 		c.weightx = 1.0;
 		c.weighty = 1.0;
 		c.gridheight = 1;
@@ -194,14 +191,14 @@ public class NavTable extends AbstractNavTable {
 		currentPosition = 0;
 		//fillValues();
 		refreshGUI();
-		super.repaint();		
+		super.repaint();
 		super.setVisible(true);
 		return true;
 
 	}
 
 	/**
-	 * It gets the alias name of the attributes if exists in the 
+	 * It gets the alias name of the attributes if exists in the
 	 * alias file
 	 * 
 	 * @param fieldName
@@ -209,14 +206,14 @@ public class NavTable extends AbstractNavTable {
 	 */
 	private String getAlias(String fieldName) {
 		File layerFile = null;
-		String filePath = null; 
+		String filePath = null;
 		String alias = null;
-		
+
 		// Added to tables without Layer support, but must be supported alias here also
-		if (layer == null) { 
+		if (layer == null) {
 			return fieldName;
 		}
-		
+
 		ReadableVectorial source = layer.getSource();
 		String pathToken = null;
 		File fileAlias = null;
@@ -237,14 +234,14 @@ public class NavTable extends AbstractNavTable {
 		}else {
 			return fieldName;
 		}
-		
+
 		//pathToken = Preferences.getAliasDir() + File.separator + layer.getName();
 		//fileAlias = new File(pathToken + ".alias");
-		
+
 		if (!fileAlias.exists()){
 			return fieldName;
 		}
-		
+
 		try {
 			String line;
 			BufferedReader fileReader = new BufferedReader(new FileReader(fileAlias));
@@ -264,17 +261,17 @@ public class NavTable extends AbstractNavTable {
 		}
 		return alias;
 	}
-	
+
 	/**
 	 * It gets the attributes names from the data table and
 	 * sets them on the left column.
 	 *
 	 */
-	private void fillAttributes(){	
+	private void fillAttributes(){
 		try {
 			String auxString = null;
 			DefaultTableModel model = (DefaultTableModel)table.getModel();
-			for (int i = 0; i < recordset.getFieldCount(); i++){									
+			for (int i = 0; i < recordset.getFieldCount(); i++){
 				Vector aux = new Vector();
 				auxString = getAlias(recordset.getFieldName(i));
 				if (auxString != null) {
@@ -282,22 +279,22 @@ public class NavTable extends AbstractNavTable {
 				}else {
 					aux.add(recordset.getFieldName(i));
 				}
-				aux.add(" ");				
+				aux.add(" ");
 				model.addRow(aux);
-				model.fireTableRowsInserted(model.getRowCount()-1, model.getRowCount()-1);				
+				model.fireTableRowsInserted(model.getRowCount()-1, model.getRowCount()-1);
 			}
-			
+
 			if (layer != null) {
 				// Geom_LENGTH
 				Vector aux = new Vector();
 				aux.add("Geom_LENGTH");
-				aux.add("0.0");		
+				aux.add("0.0");
 				model.addRow(aux);
 				model.fireTableRowsInserted(model.getRowCount()-1, model.getRowCount()-1);
 				// Geom_AREA
 				aux = new Vector();
 				aux.add("Geom_AREA");
-				aux.add("0.0");		
+				aux.add("0.0");
 				model.addRow(aux);
 				model.fireTableRowsInserted(model.getRowCount()-1, model.getRowCount()-1);
 
@@ -310,18 +307,20 @@ public class NavTable extends AbstractNavTable {
 			e.printStackTrace();
 		}
 	}
-	
+
+	@Override
 	public void fillEmptyValues() {
 		super.fillEmptyValues();
 		DefaultTableModel model = (DefaultTableModel)table.getModel();
-		for (int i = 0; i < model.getRowCount(); i++){			
+		for (int i = 0; i < model.getRowCount(); i++){
 			model.setValueAt("", i, 1);
 		}
 	}
 
+	@Override
 	public void fillValues(){
 		try {
-			
+
 			DefaultTableModel model = (DefaultTableModel)table.getModel();
 			for (int i = 0; i < recordset.getFieldCount(); i++){
 				Value value = recordset.getFieldValue(currentPosition, i);
@@ -330,9 +329,9 @@ public class NavTable extends AbstractNavTable {
 				if (value instanceof NullValue) {
 					textoValue ="";
 				}
-				model.setValueAt(textoValue, i, 1);									
+				model.setValueAt(textoValue, i, 1);
 			}
-			
+
 			if (layer != null && layer instanceof AlphanumericData) {
 				// Fill GEOM_LENGTH
 				String value = "0.0";
@@ -356,21 +355,22 @@ public class NavTable extends AbstractNavTable {
 				g = source.getShape(new Long(currentPosition).intValue());
 				source.stop();
 				geom = g.toJTSGeometry();
-				//TODO Format number  (Set units in Preferences)	
+				//TODO Format number  (Set units in Preferences)
 				value = String.valueOf(Math.round(geom.getArea()));
 				model.setValueAt(value, recordset.getFieldCount()+1, 1);
 			}
 			//refreshGUI();
-							
+
 		} catch (ReadDriverException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}	
+	}
 
-	protected Vector checkChangedValues() {
+	@Override
+	public Vector checkChangedValues() {
 		Vector changedValues = new Vector();
-		System.out.println("Number of rows: " + table.getRowCount());
+		//System.out.println("Number of rows: " + table.getRowCount());
 		for (int i=0; i<table.getRowCount()-2; i++) {
 			try {
 				String tableValue = table.getValueAt(i, 1).toString();
@@ -390,30 +390,32 @@ public class NavTable extends AbstractNavTable {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 		return changedValues;
 	}
-	
+
+	@Override
 	public void selectRow(int row){
 		table.setRowSelectionInterval(row, row);
 	}
-	
+
+	@Override
 	protected void saveRegister(){
 		//TODO check if the values type are correct
 		boolean layerEditing = true;
-		
+
 		//Stoping edition if some cell is being edited when the save button is clicked.
 		stopCellEdition();
-		
+
 		// close all windows until get the view we're working on as the active window.
 		while (!window.equals(PluginServices.getMDIManager().getActiveWindow())) {
 			PluginServices.getMDIManager().closeWindow(PluginServices.getMDIManager().getActiveWindow());
-		} 
-			int currentPos = Long.valueOf(currentPosition).intValue();
-	
-			if (layer.isWritable()) {
-			
+		}
+		int currentPos = Long.valueOf(currentPosition).intValue();
+
+		if (layer.isWritable()) {
+
 			Vector changedValues = checkChangedValues();
 			if (changedValues.size()>0) {
 				DefaultTableModel model = (DefaultTableModel)table.getModel();
@@ -422,13 +424,13 @@ public class NavTable extends AbstractNavTable {
 					layerEditing = false;
 					te.startEditing(layer);
 				}
-					
+
 				for (int i = 0; i < model.getRowCount(); i++) {
 
 					if (changedValues.contains(new Integer(i))) {
 						Object value = model.getValueAt(i, 1);
 
-						//only edit modified values, the cells that 
+						//only edit modified values, the cells that
 						//contains String instead of Value
 
 						try {
@@ -444,31 +446,32 @@ public class NavTable extends AbstractNavTable {
 						}
 					}
 				}
-				
-				if (!layerEditing)
+
+				if (!layerEditing) {
 					te.stopEditing(layer, false);
-//				layer.getMapContext().redraw();
+				}
+				//				layer.getMapContext().redraw();
 				layer.setActive(true);
 				//refreshGUI();
-			} 
-			} else {
-				JOptionPane.showMessageDialog(this, String.format(PluginServices.getText(this, "non_editable"),
-						layer.getName()));
 			}
-			//Removes the ProjectTable of this layer if it exists.
-			//Currently commented for testing purposes...
-//			ProjectExtension pe = (ProjectExtension)PluginServices.getExtension(ProjectExtension.class);
-//			Project project = pe.getProject();
-//			ArrayList views = project.getDocumentsByType("ProjectTable");
-//			for (int i=0; i < views.size(); i++){			
-//				ProjectTable pTable = (ProjectTable)views.get(i);
-//				System.out.println(i+" TableName: " + pTable.getName());
-//				if (pTable.getName().endsWith(" ET")){
-//					project.delDocument(pTable);
-//					break;
-//				}
-//			}
-		
+		} else {
+			JOptionPane.showMessageDialog(this, String.format(PluginServices.getText(this, "non_editable"),
+					layer.getName()));
+		}
+		//Removes the ProjectTable of this layer if it exists.
+		//Currently commented for testing purposes...
+		//			ProjectExtension pe = (ProjectExtension)PluginServices.getExtension(ProjectExtension.class);
+		//			Project project = pe.getProject();
+		//			ArrayList views = project.getDocumentsByType("ProjectTable");
+		//			for (int i=0; i < views.size(); i++){
+		//				ProjectTable pTable = (ProjectTable)views.get(i);
+		//				System.out.println(i+" TableName: " + pTable.getName());
+		//				if (pTable.getName().endsWith(" ET")){
+		//					project.delDocument(pTable);
+		//					break;
+		//				}
+		//			}
+
 	}
 
 	/**
@@ -478,31 +481,36 @@ public class NavTable extends AbstractNavTable {
 	protected void stopCellEdition() {
 		if (table.isEditing()) {
 			if (table.getCellEditor() != null) {
-		        table.getCellEditor().stopCellEditing();
-		    }
+				table.getCellEditor().stopCellEditing();
+			}
 		}
 	}
-	
+
+	@Override
 	public void next(){
 		stopCellEdition();
 		super.next();
 	}
-	
+
+	@Override
 	public void before(){
 		stopCellEdition();
 		super.before();
 	}
-	
+
+	@Override
 	public void last(){
 		stopCellEdition();
 		super.last();
 	}
-	
+
+	@Override
 	public void first(){
 		stopCellEdition();
 		super.first();
 	}
-	
+
+	@Override
 	public void windowClosed() {
 		stopCellEdition();
 		super.windowClosed();
