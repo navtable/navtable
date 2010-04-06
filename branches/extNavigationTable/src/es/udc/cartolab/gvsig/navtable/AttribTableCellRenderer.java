44c2d57319e6b16e0d2e32d822242dbb74430953
Pablo Sanxiao <psanxiao@icarto.es>
2010-04-06 08:02:49 +0000

    Added license to java files

--- branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AttribTableCellRenderer.java
+++ branches/extNavigationTable/src/es/udc/cartolab/gvsig/navtable/AttribTableCellRenderer.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of NavTable
+ * Copyright (C) 2009 - 2010  Cartolab (Universidade da Coruña)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ * Authors:
+ *   Juan Ignacio Varela García <nachouve (at) gmail (dot) com>
+ *   Pablo Sanxiao Roca <psanxiao (at) gmail (dot) com>
+ *   Javier Estévez Valiñas <valdaris (at) gmail (dot) com>
+ */
 package es.udc.cartolab.gvsig.navtable;
 
 import java.awt.Color;
