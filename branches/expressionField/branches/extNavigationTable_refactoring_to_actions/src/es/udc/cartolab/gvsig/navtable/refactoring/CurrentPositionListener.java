588da25e3c68400cedbb283d921c22310470490c
Francisco Puga <fpuga@cartolab.es>
2011-09-14 10:57:57 +0000

    Removed unused branch expressionField

--- branches/expressionField/branches/extNavigationTable_refactoring_to_actions/src/es/udc/cartolab/gvsig/navtable/refactoring/CurrentPositionListener.java
+++
@@ -1,9 +0,0 @@
-package es.udc.cartolab.gvsig.navtable.refactoring;
-
-import java.util.EventListener;
-
-public interface CurrentPositionListener extends EventListener {
-
-    public void onChange(CurrentPositionEvent e);
-
-}
