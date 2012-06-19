0fdf5fc27a799678507824a736218117ff031ac4
Francisco Puga <fpuga@cartolab.es>
2012-06-19 12:19:50 +0200

    refs #45
    Use only NavTableExtension to open Navtable for vectorial layers
    and also for alphanumeric tables
    
    The button will be always visible and only enabled in two situations:
     a) When the active window is an instance of BaseView and there is at
       least one vectorial layer active
     b) When the active window is a table opened from a table document. If
       is a table opened from a View Document, it's disabled.
    
    In this commit a) is working. Remains:
     1) Test that the isEnable behaviour works well for Alphanumeric Tables
     2) Remove AlphanumericNavtableExtension
     3) Refactor execute method to open as much FLyrVect as selected
     4) Refactor execute method to open Alphanumeric Tables

--- src/es/udc/cartolab/gvsig/navtable/NavTableExtension.java
+++ src/es/udc/cartolab/gvsig/navtable/NavTableExtension.java
@@ -1,21 +1,21 @@
 /*
  * This file is part of NavTable
  * Copyright (C) 2009 - 2010  Cartolab (Universidade da Coruña)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * 
+ *
+ *
  * Authors:
  *   Juan Ignacio Varela García <nachouve (at) gmail (dot) com>
  *   Pablo Sanxiao Roca <psanxiao (at) gmail (dot) com>
@@ -29,13 +29,14 @@ import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
 import com.iver.andami.preferences.IPreference;
 import com.iver.andami.preferences.IPreferenceExtension;
+import com.iver.andami.ui.mdiManager.IWindow;
 import com.iver.cit.gvsig.About;
-import com.iver.cit.gvsig.EditionUtilities;
 import com.iver.cit.gvsig.fmap.MapControl;
 import com.iver.cit.gvsig.fmap.layers.FLayer;
 import com.iver.cit.gvsig.fmap.layers.FLayers;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 import com.iver.cit.gvsig.gui.panels.FPanelAbout;
+import com.iver.cit.gvsig.project.documents.table.gui.Table;
 import com.iver.cit.gvsig.project.documents.view.gui.BaseView;
 import com.iver.utiles.extensionPoints.ExtensionPoints;
 import com.iver.utiles.extensionPoints.ExtensionPointsSingleton;
@@ -105,35 +106,40 @@ public class NavTableExtension extends Extension implements
 	Preferences p = new Preferences(configDir);
     }
 
-    public boolean isEnabled() {
-	boolean enabled = false;
-	int status = EditionUtilities.getEditionStatus();
-	if ((status == EditionUtilities.EDITION_STATUS_ONE_VECTORIAL_LAYER_ACTIVE || status == EditionUtilities.EDITION_STATUS_ONE_VECTORIAL_LAYER_ACTIVE_AND_EDITABLE)
-		|| (status == EditionUtilities.EDITION_STATUS_MULTIPLE_VECTORIAL_LAYER_ACTIVE)
-		|| (status == EditionUtilities.EDITION_STATUS_MULTIPLE_VECTORIAL_LAYER_ACTIVE_AND_EDITABLE)) {
-	    BaseView view = (BaseView) PluginServices.getMDIManager()
-		    .getActiveWindow();
-	    MapControl mapControl = view.getMapControl();
-	    FLayers flayers = mapControl.getMapContext().getLayers();
-	    int pos = flayers.getActives().length - 1;
-	    FLayer actLayer = flayers.getActives()[pos];
-	    if (actLayer instanceof FLyrVect) {
-		enabled = true;
+    protected boolean enableNavtable() {
+	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+
+	if (iWindow instanceof BaseView) {
+	    FLayer[] activeLayers = ((BaseView) iWindow).getMapControl()
+		    .getMapContext().getLayers().getActives();
+	    for (FLayer lyr : activeLayers) {
+		if (lyr instanceof FLyrVect) {
+		    return true;
+		}
 	    }
 	}
-	return enabled;
+	return false;
     }
 
-    public boolean isVisible() {
-	com.iver.andami.ui.mdiManager.IWindow f = PluginServices
-		.getMDIManager().getActiveWindow();
-	if (f == null) {
-	    return false;
-	}
-	if (f instanceof BaseView) {
+    protected boolean enableAlphanumericNavtable() {
+	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+	if ((iWindow != null) && (iWindow.getClass() == Table.class)
+		&& isAttTableFromLayer(iWindow)) {
 	    return true;
 	}
 	return false;
+    }
+
+    private boolean isAttTableFromLayer(IWindow v) {
+	return ((Table) v).getModel().getAssociatedTable() == null;
+    }
+
+    public boolean isEnabled() {
+	return enableNavtable() || enableAlphanumericNavtable();
+    }
+
+    public boolean isVisible() {
+	return true;
     }
 
     public IPreference[] getPreferencesPages() {
