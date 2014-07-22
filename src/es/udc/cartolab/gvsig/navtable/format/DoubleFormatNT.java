9c5249ba0c838aabc5aa96d1d9d2ff122970f868
Francisco Puga <fpuga@icarto.es>
2014-07-22 18:11:28 +0200

    Adds a new decimal formatter suitable to be used with BigDecimalFormat

--- src/es/udc/cartolab/gvsig/navtable/format/DoubleFormatNT.java
+++ src/es/udc/cartolab/gvsig/navtable/format/DoubleFormatNT.java
@@ -6,16 +6,25 @@ import java.util.Locale;
 
 public class DoubleFormatNT {
 
+    private static final String pattern = "0.##########";
+
     private static DecimalFormat doubleFormatOnDisplay;
     private static DecimalFormat doubleFormatOnEdit;
+    private static DecimalFormat bigDecimalFormat;
+
+    static {
+	bigDecimalFormat = (DecimalFormat) NumberFormat
+		.getNumberInstance(Locale.getDefault());
+	bigDecimalFormat.applyPattern(pattern);
+	bigDecimalFormat.setParseBigDecimal(true);
+    }
 
     public static NumberFormat getDisplayingFormat() {
 	if (doubleFormatOnDisplay == null) {
 	    doubleFormatOnDisplay = (DecimalFormat) NumberFormat
-		    .getNumberInstance(Locale
-		    .getDefault());
+		    .getNumberInstance(Locale.getDefault());
 	    // Display a maximum of 10 decimals
-	    doubleFormatOnDisplay.applyPattern("0.##########");
+	    doubleFormatOnDisplay.applyPattern(pattern);
 	}
 	return doubleFormatOnDisplay;
     }
@@ -23,11 +32,14 @@ public class DoubleFormatNT {
     public static NumberFormat getEditingFormat() {
 	if (doubleFormatOnEdit == null) {
 	    doubleFormatOnEdit = (DecimalFormat) NumberFormat
-		    .getNumberInstance(Locale
-		    .getDefault());
+		    .getNumberInstance(Locale.getDefault());
 	    doubleFormatOnEdit.setGroupingUsed(false);
 	}
 	return doubleFormatOnEdit;
+    }
+
+    public static NumberFormat getBigDecimalFormat() {
+	return bigDecimalFormat;
     }
 
 }
