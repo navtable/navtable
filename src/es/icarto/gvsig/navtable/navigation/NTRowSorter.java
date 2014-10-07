b6379954fc8ab179f7acebc43f2acb79916d4c3e
Francisco Puga <fpuga@icarto.es>
2014-10-07 20:33:40 +0200

    Clean up

--- src/es/icarto/gvsig/navtable/navigation/NTRowSorter.java
+++ src/es/icarto/gvsig/navtable/navigation/NTRowSorter.java
@@ -1,23 +1,42 @@
 package es.icarto.gvsig.navtable.navigation;
 
+import java.util.Comparator;
+
 import javax.swing.DefaultRowSorter;
 
 import org.apache.log4j.Logger;
 
 import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.hardcode.gdbms.engine.values.Value;
 import com.hardcode.gdbms.engine.values.ValueFactory;
 import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
+
+import es.udc.cartolab.gvsig.navtable.utils.ValueComparator;
 
 public class NTRowSorter<M extends SelectableDataSource> extends
 	DefaultRowSorter<M, Integer> {
 
     private static final Logger logger = Logger.getLogger(NTRowSorter.class);
     private final M model;
+    private final ValueComparator comparator;
 
     public NTRowSorter(M model) {
 	this.model = model;
 	setModelWrapper(new NTRowSorterModelWrapper());
 	setSortsOnUpdates(true); // TODO: listen for changes
+	comparator = new ValueComparator();
+    }
+
+    /**
+     * DefaultRowSorter translates all values to string and then uses
+     * Collator.getInstace() as Comparator. To avoid it a custom comparator for
+     * each column should be set using setComparator, but in our case, as the
+     * ValueComparator handles all the available object is easy to override this
+     * method
+     */
+    @Override
+    public Comparator<Value> getComparator(int column) {
+	return comparator;
     }
 
     private class NTRowSorterModelWrapper extends ModelWrapper<M, Integer> {
