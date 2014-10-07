9ca903a9599732928ed1a1362e24e1eea926a489
Francisco Puga <fpuga@icarto.es>
2014-10-07 19:42:08 +0200

    Adds a 'No Sort' class
    To avoid make an if in each method, asking if the sorter is set
    a NoRowSorter class is created. It's a mock that do nothings. A real
    sorter can be used, but this sould increase performace

---
+++ src/es/udc/cartolab/gvsig/navtable/NoRowSorter.java
@@ -0,0 +1,89 @@
+package es.udc.cartolab.gvsig.navtable;
+
+import java.util.List;
+
+import javax.swing.RowSorter;
+
+import org.apache.log4j.Logger;
+
+import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
+
+public class NoRowSorter<M extends SelectableDataSource> extends RowSorter<M> {
+
+    private static final Logger logger = Logger.getLogger(NoRowSorter.class);
+
+    private final M model;
+
+    public NoRowSorter(M model) {
+	this.model = model;
+    }
+
+    @Override
+    public M getModel() {
+	return model;
+    }
+
+    @Override
+    public void toggleSortOrder(int column) {
+    }
+
+    @Override
+    public int convertRowIndexToModel(int index) {
+	return index;
+    }
+
+    @Override
+    public int convertRowIndexToView(int index) {
+	return index;
+    }
+
+    @Override
+    public void setSortKeys(List<? extends RowSorter.SortKey> keys) {
+    }
+
+    @Override
+    public List<? extends RowSorter.SortKey> getSortKeys() {
+	return null;
+    }
+
+    @Override
+    public int getViewRowCount() {
+	try {
+	    return (int) model.getRowCount();
+	} catch (ReadDriverException e) {
+	    logger.error(e.getStackTrace(), e);
+	}
+	return 0;
+    }
+
+    @Override
+    public int getModelRowCount() {
+	return getViewRowCount();
+    }
+
+    @Override
+    public void modelStructureChanged() {
+    }
+
+    @Override
+    public void allRowsChanged() {
+    }
+
+    @Override
+    public void rowsInserted(int firstRow, int endRow) {
+    }
+
+    @Override
+    public void rowsDeleted(int firstRow, int endRow) {
+    }
+
+    @Override
+    public void rowsUpdated(int firstRow, int endRow) {
+    }
+
+    @Override
+    public void rowsUpdated(int firstRow, int endRow, int column) {
+    }
+
+}
